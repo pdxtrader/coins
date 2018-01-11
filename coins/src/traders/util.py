77d82806752937e3e2fd0de11ce18c492b538114
stavros <schampilomatis@gmail.com>
2018-01-11 09:08:09 +0100

    comment in util

--- coins/src/traders/util.py
+++ coins/src/traders/util.py
@@ -5,6 +5,9 @@ from ..proto.candlestick_pb2 import _CANDLESTICK_COIN, _CANDLESTICK_EXCHANGE
 
 
 def get_all_exchange_names() -> list:
+    """
+    get all exchange names in
+    """
     return [name for name in _CANDLESTICK_EXCHANGE.values_by_name]
 
 def get_all_coin_names() -> list:
